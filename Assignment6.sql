cc2ecd2f04f96bb441b50fd972cf5fc3479654ea
GWhitehurst <90222043+GWhitehurst@users.noreply.github.com>
2021-09-15 16:02:56 -0700

    Fixes and reformats

--- Assignment6.sql
+++ Assignment6.sql
@@ -1,6 +1,9 @@
-SELECT noOfCopies FROM tbl_book_copies WHERE branchId = (SELECT branchId FROM tbl_library_branch WHERE branchName = "Sharpstown") AND bookId = (SELECT bookId FROM tbl_book WHERE title = "The Lost Tribe");
+SELECT noOfCopies FROM tbl_book_copies
+WHERE branchId = (SELECT branchId FROM tbl_library_branch WHERE branchName = "Sharpstown")
+AND bookId = (SELECT bookId FROM tbl_book WHERE title = "The Lost Tribe");
 
-SELECT noOfCopies, branchId FROM tbl_book_copies WHERE bookId = (SELECT bookId FROM tbl_book WHERE title = "The Lost Tribe") GROUP BY branchId;
+SELECT noOfCopies, branchId FROM tbl_book_copies
+WHERE bookId = (SELECT bookId FROM tbl_book WHERE title = "The Lost Tribe") GROUP BY branchId;
 
 SELECT name FROM tbl_borrower AS a
 LEFT JOIN tbl_book_loans AS b
@@ -12,16 +15,18 @@ INNER JOIN tbl_book_loans ON tbl_book_loans.branchID = tbl_library_branch.branch
 INNER JOIN tbl_book ON tbl_book_loans.bookId = tbl_book.bookId
 INNER JOIN tbl_borrower ON tbl_book_loans.cardNo = tbl_borrower.cardNo
 WHERE tbl_library_branch.branchName = "Sharpstown"
+AND dueDate = '2021-09-15'
 GROUP BY tbl_borrower.cardNo;
 
 SELECT branchName, COUNT(dueDate) FROM tbl_library_branch
 INNER JOIN tbl_book_loans ON tbl_library_branch.branchId = tbl_book_loans.branchId
 GROUP BY branchName;
 
-SELECT name, address, COUNT(tbl_borrower.cardNo) FROM tbl_borrower
+SELECT name, address, COUNT(bookId) FROM tbl_borrower
 INNER JOIN tbl_book_loans ON tbl_borrower.cardNo = tbl_book_loans.cardNo
-WHERE COUNT(tbl_borrower.cardNo) > 5
-GROUP BY tbl_borrower.cardNo;
+GROUP BY tbl_borrower.cardNo
+HAVING COUNT(bookId) > 5
+ORDER BY tbl_borrower.cardNo;
 
 SELECT title, COUNT(tbl_book_loans.bookId) FROM tbl_author
 INNER JOIN tbl_book ON tbl_author.authorId = tbl_book.authId
